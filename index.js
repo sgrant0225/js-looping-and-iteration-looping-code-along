803aea17948e468b2eb80e57ece0697f5a7ac59d
github username <sharnellgrant@gmail.com>
2021-02-03 23:47:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( namesArray, eventName){
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+        thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`) 
+    }
+    return thankYouCards
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
